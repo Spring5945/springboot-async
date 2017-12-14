c78ec7842fbef0b5220105b63a715fb5986e1a93
nzy聂正宇 <niezy@ctrip.com>
2017-12-14 16:09:44 +0800

    readme

---
+++ SpringProject/src/main/java/com/ctrip/springaop/cglib/RealService.java
@@ -0,0 +1,11 @@
+package com.ctrip.springaop.cglib;
+
+/**
+ * Created by zhengyu.nie on 2017/12/13.
+ */
+public class RealService {
+
+    public void sayHello(){
+        System.out.println("helloworld");
+    }
+}
